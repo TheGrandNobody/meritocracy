ac6d8b51207531b7291952d10486bed6ffd0dd5d
TheGrandNobody <valuepools@gmail.com>
2020-11-21 19:08:27 +0100

    Add ProposeEvaluation, generalize voting procedure

--- contracts/governance/ValueSenate.sol
+++ contracts/governance/ValueSenate.sol
@@ -43,6 +43,15 @@ contract ValueSenate is Ownable {
     }
 
     /**
+     * @notice All possible proposals that can be made
+     */
+    enum Proposal {
+        Trade,
+        Success,
+        Update
+    }
+
+    /**
      * @notice Info of Value Feed trade proposals.
      */
     struct TradeProposal {
@@ -69,6 +78,10 @@ contract ValueSenate is Ownable {
      */
     struct UpdateProposal {
         uint256 id;       // ID of the update proposal
+        uint256 startTime;               // The unix time at which the voting period for this proposal starts
+        uint256 endTime;                 // The unix time at which the voting period for this proposal ends
+        uint256 votesFor;                // The number of votes for the proposal
+        uint256 votesAgainst;            // The number of votes against the proposal
 
         address proposer; // Address of the proposer
 
@@ -79,14 +92,17 @@ contract ValueSenate is Ownable {
 
     struct SuccessProposal {
         uint256 id;                      // ID of the success proposal
+        uint256 tradeId;                 // ID of the initial trade proposal
+        uint256 withdrawalId;            // ID of the withdrawal trade proposal
         uint256 startTime;               // The unix time at which the voting period for this proposal starts
         uint256 endTime;                 // The unix time at which the voting period for this proposal ends
         uint256 votesFor;                // The number of votes for the proposal
         uint256 votesAgainst;            // The number of votes against the proposal
+        
+        bool aiVote;                     // The vote of the AI;
 
         mapping (address => bool) voted; // Record of users who have voted
 
-        uint256[] proposals;             // Array of ids for the proposals this success proposal represents
         State state;                     // The state of the proposal
     }
 
@@ -116,26 +132,27 @@ contract ValueSenate is Ownable {
     uint256 public lastSuccessId;
 
     event Voted(address indexed voter, uint256 id, bool pro);
-    event TradeProposalInitiated(uint256 id, address indexed proposer, address valuePool, address targetAsset, uint256 startTime, uint256 endTime, bool second);
+    event TradeProposalInitiated(uint256 id, address indexed proposer, address indexed valuePool, address indexed targetAsset, uint256 startTime, uint256 endTime, bool second);
     event TradeProposalFinalized(uint256 id, uint256 time, bool passed);
     event TradeWithdrawal(uint256 id);
+    event SuccessProposalInitiated(uint256 id, uint256 startTime, uint256 endTime);
 
     /// @notice The EIP-712 typehash for the contract's domain
     bytes32 public constant EIP_DOMAIN_TYPEHASH = keccak256("EIPDomain(string contractName, uint256 chainId, address contractAddress)");
 
     /// @notice The EIP-712 typehash for the voting struct used by the contract
-    bytes32 public constant VOTE_STRUCT_TYPEHASH = keccak256("Vote(address voter, uint256 id, bool pro)");
+    bytes32 public constant VOTE_STRUCT_TYPEHASH = keccak256("Vote(address voter, uint256 id, bool pro, Proposal type)");
 
     /**
      * @notice Initiates a trade proposal by a given user so as to trade the asset of a value pool for a target asset
-     * @param proposer The ETH address of the specified user making the proposal
-     * @param valuePool The ERC20 address of the asset belonging to the specified value pool
-     * @param targetAsset The ERC20 address of the specified target token for which the value pool's contents will be traded
-     * @param second Determines whether this is the first (trade for) or second (trade back for) round of the proposal
+     * @param _proposer The ETH address of the specified user making the proposal
+     * @param _valuePool The ERC20 address of the asset belonging to the specified value pool
+     * @param _targetAsset The ERC20 address of the specified target token for which the value pool's contents will be traded
+     * @param _second Determines whether this is the first (trade for) or second (trade back for) round of the proposal
      */
-    function proposeTrade(address proposer, address valuePool, address targetAsset, bool second) external  {
-        require(valueFeed.viewMeritScore(proposer) + value.viewDelegateVotes(proposer) >= 4e5, "ValueSenate::proposeTrade: User is not competent enough to propose"); 
-        require(valueFeed.viewSwapped(valuePool) == second, "ValueSenate::proposeTrade: Value Pool is not in the right state");
+    function proposeTrade(address _proposer, address _valuePool, address _targetAsset, bool _second) external  {
+        require(valueFeed.viewMeritScore(_proposer) + value.viewDelegateVotes(_proposer) >= 4e5, "ValueSenate::proposeTrade: User is not competent enough to propose"); 
+        require(valueFeed.viewSwapped(_valuePool) == _second, "ValueSenate::proposeTrade: Value Pool is not in the right state");
 
         TradeProposal storage proposal = tradeProposals[lastTradeId];
         proposal.id = lastTradeId;
@@ -143,30 +160,46 @@ contract ValueSenate is Ownable {
     
         proposal.startTime = block.timestamp;
         proposal.endTime = proposal.startTime.add(86400);
-        proposal.proposer = proposer;
-        proposal.votesFor = valueFeed.viewMeritScore(proposer) + value.viewDelegateVotes(proposer);
-        proposal.voted[proposer] = true;
-        proposal.sourceToken = valuePool;
-        proposal.targetAsset = targetAsset;
+        proposal.proposer = _proposer;
+        proposal.votesFor = valueFeed.viewMeritScore(_proposer) + value.viewDelegateVotes(_proposer);
+        proposal.voted[_proposer] = true;
+        proposal.sourceToken = _valuePool;
+        proposal.targetAsset = _targetAsset;
 
-        proposal.withdrawal = second;
+        proposal.withdrawal = _second;
         
         proposal.state = State.InProgress;   
 
-        emit TradeProposalInitiated(proposal.id, proposer, valuePool, targetAsset, proposal.startTime, proposal.endTime, second);
+        emit TradeProposalInitiated(proposal.id, _proposer, _valuePool, _targetAsset, proposal.startTime, proposal.endTime, _second);
+    }
+
+    function proposeEvaluation(uint256 _tradeId, uint256 _withdrawalId, bool _internalEval) public onlyOwner {
+        SuccessProposal storage proposal = successProposals[lastSuccessId];
+
+        proposal.id = lastSuccessId;
+        lastSuccessId = lastSuccessId.add(1);
+        proposal.startTime = block.timestamp;
+        proposal.endTime = proposal.startTime.add(86400);
+        proposal.aiVote = _internalEval;
+        proposal.tradeId = _tradeId;
+        proposal.withdrawalId = _withdrawalId;
+
+        proposal.state = State.InProgress;
+
+        emit SuccessProposalInitiated(proposal.id, proposal.startTime, proposal.endTime);
     }
 
     /**
-     * @notice (Intermediary) votes on a given trade proposal for a user
+     * @notice (Intermediary) votes on a given proposal for a user
      * @param _id The ID of the specified trade proposal
      * @param _pro Whether the user is for (true) or against (false) the trade proposal
      */
-    function voteOnTradeProposal(uint256 _id, bool _pro) public {
-        _voteOnTradeProposal(_id, msg.sender, _pro);
+    function voteOnProposal(uint256 _id, bool _pro, Proposal _type) public {
+        _voteOnProposal(_id, msg.sender, _pro, _type);
     }
 
     /**
-     * @notice (Intermediary) votes on a given trade proposal for a signee
+     * @notice (Intermediary) votes on a given proposal for a signee
      * @param _id The ID of the specified trade proposal
      * @param _pro Whether the user is for (true) or against (false) the trade proposal
      * @param _r Half of the ECDSA signature pair
@@ -174,42 +207,90 @@ contract ValueSenate is Ownable {
      * @param _v The recovery byte of the signature
      * @dev Taken and changed from Comp.sol
      */
-    function voteOnTradeProposalBySignature(uint _id, bool _pro, bytes32 _r, bytes32 _s, uint8 _v) public {
+    function voteOnProposalBySignature(uint _id, bool _pro, Proposal _type, bytes32 _r, bytes32 _s, uint8 _v) public {
         bytes32 domain = keccak256(abi.encode(EIP_DOMAIN_TYPEHASH, keccak256(bytes("Value")), getChainId(), address(this)));
-        bytes32 delegationHash = keccak256(abi.encode(VOTE_STRUCT_TYPEHASH, _id, _pro));
+        bytes32 delegationHash = keccak256(abi.encode(VOTE_STRUCT_TYPEHASH, _id, _pro, _type));
         bytes32 msgDigest = keccak256(abi.encodePacked("\x19\x01", domain, delegationHash));
         address signee = ecrecover(msgDigest, _v, _r, _s);
 
-        require(signee != address(0), "ValueToken::voteOnTradeProposalBySignature: invalid address");
+        require(signee != address(0), "ValueToken::voteOnProposalBySignature: invalid address");
         
-        _voteOnTradeProposal(_id, signee, _pro);
+
+        _voteOnProposal(_id, signee, _pro, _type);
     }
 
     /**
-     * @notice Votes on a given trade proposal for a given user
+     * @notice Votes on a given proposal for a given user
      * @param _id The ID of the specified trade proposal
      * @param _voter The ETH address of the specified user
      * @param _pro Whether the user is for (true) or against (false) the trade proposal
      * @dev Inspired from Comp.sol
      */
-    function _voteOnTradeProposal(uint256 _id, address _voter, bool _pro) private {
-        TradeProposal storage proposal = tradeProposals[_id];
-        require((proposal.state == State.InProgress)
-             && (proposal.startTime <= block.timestamp) 
-             && (proposal.endTime > block.timestamp), "ValueSenate::_voteOnTradeProposal: Proposal is not in state for voting");
+    function _voteOnProposal(uint256 _id, address _voter, bool _pro, Proposal _type) private {
+        State state;
+        uint256 startTime;
+        uint256 endTime;
+        uint256 votesFor;
+        uint256 votesAgainst;
+        bool voted;
+
+        if (_type == Proposal.Trade) {
+            TradeProposal storage proposal = tradeProposals[_id];
+            state = proposal.state;
+            startTime = proposal.startTime;
+            endTime = proposal.endTime;
+            voted = proposal.voted[_voter];
+            votesFor = proposal.votesFor;
+            votesAgainst = proposal.votesAgainst;
+        } else if (_type == Proposal.Update) {
+            UpdateProposal storage proposal = updateProposals[_id];
+            state = proposal.state;
+            startTime = proposal.startTime;
+            endTime = proposal.endTime;
+            voted = proposal.voted[_voter];
+            votesFor = proposal.votesFor;
+            votesAgainst = proposal.votesAgainst;
+        } else if (_type == Proposal.Success) {
+            SuccessProposal storage proposal = successProposals[_id];
+            state = proposal.state;
+            startTime = proposal.startTime;
+            endTime = proposal.endTime;
+            voted = proposal.voted[_voter];
+            votesFor = proposal.votesFor;
+            votesAgainst = proposal.votesAgainst;
+        }
+
+        require((state == State.InProgress)
+             && (startTime <= block.timestamp) 
+             && (endTime > block.timestamp), "ValueSenate::_voteOnTradeProposal: Proposal is not in state for voting");
         require(valueFeed.viewTotalAmount(_voter) > 0, "ValueSenate::_voteOnTradeProposal: User is not a contributor to the Value Feed");
-        require(!proposal.voted[_voter], "ValueSenate::_voteOnTradeProposal: User has already voted");
+        require(!voted, "ValueSenate::_voteOnTradeProposal: User has already voted");
         require(value.viewDelegate(_voter) == _voter, "ValueSenate::_voteOnTradeProposal");
 
         uint256 numberOfVotes = valueFeed.viewMeritScore(_voter).add(value.viewDelegateVotes(_voter));
 
         if (_pro) {
-            proposal.votesFor = proposal.votesFor.add(numberOfVotes);
+            votesFor = votesFor.add(numberOfVotes);
         } else {
-            proposal.votesAgainst = proposal.votesAgainst.add(numberOfVotes);
+            votesAgainst = votesAgainst.add(numberOfVotes);
         }
 
-        proposal.voted[_voter] = true;
+        if (_type == Proposal.Trade) {
+            TradeProposal storage proposal = tradeProposals[_id];
+            proposal.voted[_voter] = true;
+            proposal.votesFor = votesFor;
+            proposal.votesAgainst = votesAgainst;
+        } else if (_type == Proposal.Update) {
+            UpdateProposal storage proposal = updateProposals[_id];
+            proposal.voted[_voter] = true;
+            proposal.votesFor = votesFor;
+            proposal.votesAgainst = votesAgainst;          
+        } else if (_type == Proposal.Success) {
+            SuccessProposal storage proposal = successProposals[_id];
+            proposal.voted[_voter] = true;
+            proposal.votesFor = votesFor;
+            proposal.votesAgainst = votesAgainst;
+        }
 
         emit Voted(_voter, _id, _pro);
     }
@@ -224,15 +305,11 @@ contract ValueSenate is Ownable {
         uint256 percentageAgainst = proposal.votesAgainst.mul(100).div(total);
         uint256 timeOfExecution = block.timestamp;
 
-        if (percentageFor > percentageAgainst) {
-            proposal.state = State.Passed;
-            proposal.timeOfExecution = timeOfExecution.add(2592000);
-            emit TradeProposalFinalized(_id, timeOfExecution, true);
-        } else {
-            proposal.state = State.Failed;
-            emit TradeProposalFinalized(_id, timeOfExecution, false);
-        }
+        proposal.timeOfExecution = proposal.withdrawal ? timeOfExecution : timeOfExecution.add(2592000);
+        proposal.state = percentageFor > percentageAgainst ? State.Passed : State.Failed;
+        emit TradeProposalFinalized(_id, timeOfExecution, percentageFor > percentageAgainst);
     }
+
 
     function executeTradeProposal(uint256 _id, address[] memory _path, bool _ethTrade, bool _tokenForEth) public {
         TradeProposal storage proposal = tradeProposals[_id];
@@ -250,12 +327,15 @@ contract ValueSenate is Ownable {
         proposal.state = State.Executed;
 
         if (proposal.withdrawal) {
-            emit TradeWithdrawal(proposal.id);
+            TradeWithdrawal(_id);
         }
     }
 
-    function evaluate(uint256 _tradeId, uint256 _withdrawId, uint256 _internalEval) public onlyOwner {
+    function evaluate(uint256 _id) public onlyOwner {
+        SuccessProposal storage proposal = successProposals[_id];
+
         
+
     }
 
     
